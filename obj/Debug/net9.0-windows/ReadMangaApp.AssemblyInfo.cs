8c70e147d5d563b0992fbbd6576f0f1df5856101
keyshii <satonina.alyona@yandex.ru>
2025-06-12 21:06:33 +0300

    Версия приложения с отображением названия коллекции манги в каталоге

--- obj/Debug/net9.0-windows/ReadMangaApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/ReadMangaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadMangaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc22acc1f92eeccf71aaec1333450a69205d4b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bdaf55ca2bf2de5916dcf80ff45f621e5f2143")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadMangaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadMangaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
